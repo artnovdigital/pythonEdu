1b7a1cdf0a4c885c01326e14fc5eca3ed065ef97
Artem <artnovdigital@gmail.com>
2023-07-15 09:03:39 +0200

    Save changes

---
+++ edu.py
@@ -0,0 +1,217 @@
+
+
+n = int(input("Enter length: "))
+
+user_list = []
+
+i = 0
+
+while i < n:
+    string = "Enter data #" + str(i + 1) + ":"
+    user_list.append(input(string))
+    i += 1
+print (user_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n = int(input("Enter length: "))
+
+# user_list = []
+
+# i = 0
+# while i < n:
+    # string = "Enter element #" + str(i + 1) + ": "
+    # user_list.append(input(string))
+    # i += 1
+    
+# print(user_list)
+
+
+# nums = [5, 2, 7, "50", False]
+
+# for el in nums:
+    # el *= 2
+    # print(el)
+
+
+# numbers = [5, 2, 7]
+# # numbers[3] = 100
+# numbers.append(100)
+# numbers.insert(1, True)
+# b = [5, 6, 8]
+# numbers.extend(b)
+# numbers.sort()
+# # numbers.reverse()
+# numbers.pop(-2)
+# numbers.remove(True)
+# # numbers.clear()
+# print(numbers.count(True))
+# print(len(numbers))
+
+
+# nums = [5, 7, 2, 4, 7, True, "Hello", 6.7, [5,7]]
+
+# nums[0] = 50
+# nums[5] = 1.01
+# print(nums[-1][1])
+
+
+
+# found = None
+# for i in "hello":
+    # if i == "o":
+        # found = True
+        # break
+    # else:
+        # found = False
+
+# print(found)
+
+
+
+# for i in range(1, 11):
+    # if i == 5:
+        # break
+    # if i % 2 == 0:
+        # continue
+    # print(i)
+
+
+# isHasCar = True
+# while isHasCar:
+    # if input("Enter data: ") == "stop":
+        # isHasCar = False
+        # print ("Stopped")
+    # else:
+        # print ("Wrong")
+        
+
+
+# i = 5
+# while i <= 15:
+    # print(i)
+    # i += 2
+
+
+# count = 0
+# word = "hello world!"
+# for i in word:
+    # if i == "w":
+        # count += 1
+# print(count)
+
+
+
+
+# for i in range(1, 6, 2):
+    # print(i)
+
+# data = input()
+
+# number = 5 if data == "five" else 0
+
+# if data == "five":
+    # number = 5
+# else:
+    # number = 0
+# print(number)
+    
+
+
+
+
+# user_data = int(input("Enter your number: "))
+
+# isHappy = False
+
+# if isHappy or user_data == 6 or not isHappy:
+
+    # print("User is happy")
+    
+# elif user_data == 5:
+
+    # print("Equal 5")
+    
+# elif user_data == 7:
+    
+    # print("Equal 7")
+
+# else: 
+    
+    # print ("User is unhappy")
+
+
+# user_data = int(input("Enter your number: "))
+
+# isHappy = True
+
+# if not isHappy:
+    # print("User is very happy!")
+# elif user_data != 5:
+    # print ("Number is not 5")
+# else:
+    # print("User is unhappy!")
+    
+# # user_data = int(input("Enter your number: "))
+# # if user_data != 5:
+    # # print("Not equal 5")
+# # if user_data > 5:
+    # # print ("Number is bigger than 5")
+# # if user_data < 5:
+    # # print ("Number is less than 5")
+# # if user_data == 5:
+    # # print("Now it is 5")
+
+
+
+# # # print ('Hellow world', 7, sep="", end="!\n")
+# # # print ('Some \' \t \\ text')
+# # # Comment
+# # plus = 3
+# # # print ('Result:', 5//plus)
+# # print ('Result:', abs(min(5,-3,2,10,1))//plus)
+# # input ('Enter you edge')
+# # number = 5
+# # string = 'text'
+
+# # print (string, end='')
+
+
+
+# # digit = 4.5
+
+# # print (digit)
+
+# # boolean = True
+
+# # print (boolean)
+
+# # print (string + str(boolean))
+
+# # str_num = '5'
+
+# # print (string + str(digit + int(str_num)))
+
+# # num1 = int(input ('Input first number: '))
+# # num2 = int(input ('Second number: '))
+
+# # num2 -= 1
+
+# # result = (num1 + num2)
+# # print (result)
+
+# # word = 'Hi'
+
+# # print ((word + '.') * result, end=" ")
